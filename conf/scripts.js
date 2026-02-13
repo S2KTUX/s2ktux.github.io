26b9251d4ef40748b633d83b36e3ec784c9acab1
S2KTUX <alaisysadm@gmail.com>
2026-02-14 00:48:02 +0100

    Update scripts.js

--- conf/scripts.js
+++ conf/scripts.js
@@ -39,3 +39,125 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 });
+
+
+
+/* ===== TERMINAL ===== */
+
+document.addEventListener("DOMContentLoaded", function(){
+
+const terminal = document.getElementById("terminal-body");
+if(!terminal) return; // evita errores si no existe
+
+function print(text, type="output"){
+    const line = document.createElement("div");
+    line.className = type;
+    line.textContent = text;
+    terminal.appendChild(line);
+}
+
+/* mensaje inicial */
+
+function boot(){
+
+    print("Bienvenido a s2ktux");
+    print("---------------------------------------");
+    print("Aquí podrás aprender sobre Linux");
+    print("Preparación para certificados LPIC y RHCSA");
+    print("Y proyectos pequeños...");
+    print('Escribe "help" para comenzar.');
+}
+
+/* crear input */
+
+function createInput(){
+
+    const wrapper = document.createElement("div");
+    wrapper.className = "input-line";
+
+    const prompt = document.createElement("span");
+    prompt.className = "prompt";
+    prompt.textContent = "s2ktux@web:~$";
+
+    const input = document.createElement("input");
+    input.className = "terminal-input";
+    input.placeholder = "Escribe algo... (ej: help)";
+
+    wrapper.appendChild(prompt);
+    wrapper.appendChild(input);
+    terminal.appendChild(wrapper);
+
+    input.focus();
+
+    input.addEventListener("keydown", function(e){
+
+        if(e.key === "Enter"){
+
+            const cmd = input.value.trim();
+            input.disabled = true;
+
+            runCommand(cmd);
+
+            createInput();
+            terminal.scrollTop = terminal.scrollHeight;
+        }
+    });
+}
+
+/* comandos */
+
+function runCommand(cmd){
+
+    if(cmd === "help"){
+        print("");
+        print("Comandos disponibles:");
+        print("help");
+        print("about");
+        print("cursos");
+        print("proyectos");
+        print("whoami");
+        print("pwd");
+        print("sudo");
+        print("clear");
+    }
+
+    else if(cmd === "about"){
+        print(" Trabajo en el area de sistemas desde hace años y me gusta mucho Linux. Al mismo tiempo que aprendo, me gusta compartirlo para todo aquel que esté siguiendo mi mismo camino");
+    }
+
+    else if(cmd === "cursos"){
+        print("Actualmente cuento con el curso de LPIC1 y RHCSA. RHCSA está tambien en el canal de Youtube!!");
+    }
+
+    else if(cmd === "proyectos"){
+        print("Proximamente..");
+    }
+
+    else if(cmd === "clear"){
+        terminal.innerHTML="";
+        boot();
+    }
+
+   else if(cmd === "whoami"){
+    print("s2ktux");
+} 
+
+    else if(cmd === "pwd"){
+    print("/home/s2ktux");
+}
+
+    else if(cmd === "sudo"){
+    print("Buen intento 😄", "error!");
+}
+
+    else if(cmd !== ""){
+        print("Command not found: "+cmd,"error");
+    }
+}
+
+/* iniciar terminal */
+
+boot();
+createInput();
+
+};
